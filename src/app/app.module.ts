44728ac4591ba42b2eed400dd6e50f7535447503
wanjiku <ciiku92@gmail.com>
2020-01-17 14:33:04 +0300

    Create repo-search component to enable user to search github repositories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { RepoSearchComponent } from './repo-search/repo-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    RepoSearchComponent
   ],
   imports: [
     BrowserModule,
--- src/app/user-class.ts
+++ src/app/user-class.ts
@@ -1,2 +1,3 @@
 export class UserClass {
+    constructor(public id: number, public login: string, public avatar_url: string) { }
 }
