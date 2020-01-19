c42331915e48986f52fbc460563d561309866e60
wanjiku <ciiku92@gmail.com>
2020-01-19 17:55:11 +0300

    Create date-format pipe to format date of repo creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { RepoSearchComponent } from './repo-search/repo-search.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RepoComponent } from './repo/repo.component';
+import { DateFormatPipe } from './date-format.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    RepoSearchComponent
+    RepoSearchComponent,
+    NotFoundComponent,
+    RepoComponent,
+    DateFormatPipe
   ],
   imports: [
     BrowserModule,
--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersHttpServiceService } from '../services/users-http-service.service';
+import { UserClass } from '../user-class';
+import { RepoClass } from '../repo-class';
+
 
 @Component({
   selector: 'app-repo',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepoComponent implements OnInit {
 
-  constructor() { }
+  users: UserClass;
+  repos: RepoClass[] = [];
+
+  constructor(public userHttpService: UsersHttpServiceService) { }
 
   ngOnInit() {
+    this.searchUser('robertmuthuri');
+    this.getRepos('robertmuthuri');
   }
+
+  searchUser(searchTerm) {
+
+    this.userHttpService.searchUsers(searchTerm).then(
+      (success) => {
+        this.users = this.userHttpService.user;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getRepos(searchTerm) {
+    this.userHttpService.getRepos(searchTerm).then(
+      (results) => {
+        this.repos = this.userHttpService.repos;
+        console.log(this.repos);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
 
 }
