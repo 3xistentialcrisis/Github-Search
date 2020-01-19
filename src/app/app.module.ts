104e6507197d9cb84e53c73e15cee0ede0958de0
wanjiku <ciiku92@gmail.com>
2020-01-19 20:36:39 +0300

    import MDBBootstrapModule after install of angular-bootstrap-md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
@@ -8,6 +11,8 @@ import { RepoSearchComponent } from './repo-search/repo-search.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { RepoComponent } from './repo/repo.component';
 import { DateFormatPipe } from './date-format.pipe';
+import { RepoNavbarComponent } from './repo-navbar/repo-navbar.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
@@ -16,11 +21,15 @@ import { DateFormatPipe } from './date-format.pipe';
     RepoSearchComponent,
     NotFoundComponent,
     RepoComponent,
-    DateFormatPipe
+    DateFormatPipe,
+    RepoNavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    MDBBootstrapModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
