7f3571ce9539a2b408cb9211481f233d1ae1cc29
wanjiku <ciiku92@gmail.com>
2020-01-19 18:16:24 +0300

    Add routes to repocomponent and notfoundcomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { RepoComponent } from './repo/repo.component';
+import { UserComponent } from './user/user.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: RepoComponent},
+  { pathMatch: 'full', redirectTo: 'home', path: '' },
+  { path: '**', component: NotFoundComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
